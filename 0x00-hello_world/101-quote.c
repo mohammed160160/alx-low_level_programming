bdee05434cca8b42a75142e47a563c84de0b4954
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-10 21:42:21 +0000

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/*
+ * main - writing a string
+ * Description - writing a string of text without printf
+ * or puts
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -
+		    Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
