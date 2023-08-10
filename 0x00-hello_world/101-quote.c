2c98c256522a24542a8d34ee9646f7092d074905
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-10 21:55:43 +0000

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * or puts
  * Return: 1
  */
-int main(void)
+	int main(void)
 {
 	write(2, "and that piece of art is useful\" -
-		    Dora Korpar, 2015-10-19\n", 58);
+		    Dora Korpar, 2015-10-19\n");
 	return (1);
 }
