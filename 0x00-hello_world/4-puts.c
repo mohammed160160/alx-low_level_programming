99eadedd73a6353d6847879bf3cbdc0e789d312c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-10 15:11:40 +0000

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - used to print out a string
+ * Description - a starting method to printout a
+ * string using puts
+ * Return: 0
+ */
+int main(void)
+{
+	char puts('"Programming is like building a multilingual puzzle\n');
+	return (0);
+}
