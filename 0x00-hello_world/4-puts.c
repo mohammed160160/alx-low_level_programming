bc365840574907499db93c953c5af39bbe8ea88c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-10 16:44:46 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	char puts('"Programming is like building a multilingual puzzle\n');
-	return (0);
+	char string[] = ""Programming is like building a multilingual puzzle\n"
+		puts(string);
+	return 0;
 }
