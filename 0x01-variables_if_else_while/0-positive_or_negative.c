1b322976ec42a4151ac14710921d9476b132fba4
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-11 13:38:55 +0000

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main -for printing if n is positive, negative or zero
  * Description -this is an if function that that defines if n is
