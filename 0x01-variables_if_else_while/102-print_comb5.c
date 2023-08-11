8020746ad98cbfefd226d6c46e2f1c00ad2545d8
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-11 21:52:33 +0000

    task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - writes a combination of each 2 valued numbers
+ * Description - this program is used to print the value of each combination
+ * of numbers from 00 to 99
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+	int j;
+
+	while (i <= '98')
+	{
+	j = i + 1;
+	while (j <= 99)
+	{
+	putchar(i / 10 + '0');
+	putchar(i % 10 + '0');
+	putchar(' ');
+	putchar(j / 10 + '0');
+	putchar(j % 10 + '0');
+	if (i != 98 || j != 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	j++;
+	}
+	i++;
+	}
+	return (0);
+	}
