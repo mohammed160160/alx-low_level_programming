22977906ad84d7dac243bc6228e8de4eaa278558
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-11 20:53:04 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - to print from 0 to 9 and place comma and spaces
+ * Description - this is a program to type from 0 to 9 and print' ,' next each
+ * Return: 0
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+	putchar(x);
+	if (x != 9)
+	{
+	putchar(',')
+	putchar(' ')
+	}
+	x++;
+	}
+	putchar('\n');
+	return (0);
+
