a5df9b05d33b42c09a3f152fbcd5809998ae2f8d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 19:06:30 +0000

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,20 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-	{
-	int n;
-
-	n = add(89, 9);
-	printf("%d\n", n);
-	return (0);
-	}
 /**
  * add - adds 2 numbers
  * @a: first integer
