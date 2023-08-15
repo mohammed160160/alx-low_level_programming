8155ff804bc09c5edd4ba9e360b6f06ce1e330db
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 16:15:55 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,16 @@
  * Return: Always 0.
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 	{
 	int x = 97;
 
 	while (x <= 122)
 	{
-	putchar(x);
+	_putchar(x);
 	x++;
 	}
-	putchar('\n');
+	_putchar('\n');
 	return;
 	}
 
