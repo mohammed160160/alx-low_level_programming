306ef7caa485d35d9988bab510e4b2f072c5fdba
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 23:18:05 +0000

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - writes the time table from 0 to n
+ * @n: the number of the time table
+ * Return: Always 0.
+ */
+
+void print_times_table(int n)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= n ; x++)
+	{
+	_putchar('0');
+	for (y = 1; y <= n ; y++)
+	{
+	z = x * y;
+	_putchar(',');
+	_putchar(' ');
+	if (z <= 9)
+	{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(z + '0');
+	}
+	else if (z <= 99 && z>= 10)
+	{
+	_putchar(' ');
+	_putchar(z / 10 + '0');
+	_putchar(z % 10 + '0');
+	}
+	else if (z >= 100)
+	{
+	_putchar(z / 100 + '0');
+	_putchar(z / 10 /10 + '0');
+	_putchar(z % 10 %10 + '0');
+	}
+	_putchar('\n');
+	}
+	}
+	return;
+	}
