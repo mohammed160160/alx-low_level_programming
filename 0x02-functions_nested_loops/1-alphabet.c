3ef464e5fae4f2487fac026afe4007f98ce5a989
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 12:07:07 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - types smallcase alphabet
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+	{
+	int x = 97;
+
+	while (x <= 122)
+	{
+	putchar(x)
+	x++
+	}
+	putchar('\n')
+	return (0);
+	}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
