a0f93abae54557739a9578ca1087e3af0f329d76
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 20:01:30 +0000

    tasks 3 and 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isalpha - write if the written character is alphabet(upper or lower)
+ * @c: the character the checker uses
+ * Return: 1 if it's an alphabet value or 0 if it's not
+ */
+int _isalpha(int c)
+	{
+	if (c >= 97 && c <= 112)
+	{
+	return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	}
