fe3f985b0f941d03e61e036548b46feb4e3d6eba
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 13:56:57 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - to write the alphabet from a to z
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+	{
+	int x = 97;
+
+	while (x <= 122)
+	{
+	putchar(x);
+	x++;
+	}
+	putchar('\n');
+	return;
+	}
+
+int main(void)
+	{
+	print_alphabet();
+	return (0);
+	}
