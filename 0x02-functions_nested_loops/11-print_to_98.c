e9046e6baf70410d1d7596c08fd7fcb4d5d584e4
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 22:47:27 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_to_98 - writes from the number given to 98
+ * @n: defines the starting points
  * Return: Always 0.
  */
-int main(void)
-	{
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
-	}
-
 void print_to_98(int n)
 	{
-
+	while (n != 98)
+	{
+	if (n < 98)
+	{
+	for (n = n; n < 98; n++)
+	{
+	printf("%d, ", n);
+	}
+	}
+	else if (n > 98)
+	{
+	for (n = n; n > 98; n--)
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	printf("98\n");
+	return;
 	}
