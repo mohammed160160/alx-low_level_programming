a5222977595fcc5b278b7eb08cb8f3ac1efe04fe
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 17:35:11 +0000

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9 ; x++)
+	{
+	_putchar('0');
+	for (y = 1; y <= 9 ; y++)
+	{
+	z = x * y;
+	_putchar(',');
+	_putchar(' ');
+	if (z <= 9)
+	{
+	_putchar(' ');
+	_putchar(z + '0');
+	}
+	else
+	{
+	_putchar(z / 10 + '0');
+	_putchar(z % 10 + '0');
+	}
+	}
+	_putchar('\n');
+	}
+	return;
+	}
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
