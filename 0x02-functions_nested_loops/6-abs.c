ff36490ae1a4ecd5a7f0280dea4b621459e9474c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-15 20:39:01 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - checks if the char is positive, negative or zero
+ * @n: the value the checker do
+ * Return: 1 if positive, -1 if negative , 0 if zero
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+	}
