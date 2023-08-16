64c2615e15142e4fcf20e9402a2b32f58622bc3f
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-16 15:38:17 +0000

    task 14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,9 +7,14 @@ int main(void)
 {
 int x;
 unsigned long v;
+unsigned long v1;
+unsigned long v2;
 unsigned long p;
+unsigned long p1;
+unsigned long p2;
+unsigned long l;
 
-
+l = 1000000000000;
 v = 1;
 p = 0;
 
@@ -21,6 +26,19 @@ p = v - p;
 printf(", ");
 }
 
+v1 = v / l;
+v2 = v % l;
+p1 = p / l;
+p2 = p % l;
+
+for (x = 0; x <= 5; x++)
+{
+v1 += p1;
+v2 += p2;
+printf("%lu%lu", v1, v2)
+p1 = v1 - p1
+p2 = v2 - p2
+}
 printf("\n");
 return (0);
 }
