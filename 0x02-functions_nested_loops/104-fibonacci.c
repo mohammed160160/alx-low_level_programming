97f29ff57487f18720397f4ec044b499b8db21a8
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-16 18:54:38 +0000

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,8 +10,8 @@ unsigned long l, lx;
 unsigned long v1, v2, p1, p2;
 unsigned long v, p;
 
-l = 1000000000000;
-lx = 10000000000000;
+l = 1000000000;
+lx = 10000000000;
 v = 1;
 p = 0;
 for (x = 0; x <= 91; x++)
@@ -27,11 +27,15 @@ p2 = p % l;
 for (x = 0; x <= 5; x++)
 {
 v1 += p1;
-v2 += p2;
-if (v2 >= lx)
+if (v2 + p2 >= lx)
 {
+v2 = v2 + p2;
 v2 = v2 - lx;
 v1++;
+}
+else 
+{
+v2 = v2 + p2;
 }
 printf("%lu%lu", v1, v2);
 p1 = v1 - p1;
