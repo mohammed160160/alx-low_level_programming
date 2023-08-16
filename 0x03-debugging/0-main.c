3ea1e11a0e6601f765a75156416665e63fa5dfb2
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-16 12:23:53 +0000

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - determains if the number i is positive,
+ * negative or zero
+ * @i: the number which the function determains
+ * Return: 0
+*/
+
+void positive_or_negative(int i);
+
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+else
+{
+printf("0 is zero\n");
+}
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is a directory to debug some functions
