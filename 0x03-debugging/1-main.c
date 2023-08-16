cd0ec5a68022c6c878213c3e922c1fada45211dc
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-16 13:00:44 +0000

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-int i;
+	int i;
 
-printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-i = 0;
+	i = 0;
 
-while (i < 10)
-{
-putchar(i);/*there should be an increment value here*/
-}
+	while (i < 10)/*there should be an increment function*/
+	{
+		putchar(i);/*this put char shouldnt be here*/
+	}	
 
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+	return (0);
 }
