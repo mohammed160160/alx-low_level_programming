d5ea1737d8b93828f01b5a7962744b17830836eb
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-17 16:45:21 +0000

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_line - to write a line equal to the number n
+ * print_diagonal - to write a diagonal line equal to the number n
  * @n : the length of the line
  * Return: Always 0.
  */
 
-void print_line(int n)
-        {
-        int x;
+void print_diagonal(int n)
+	{
+	int x;
+	int y;
 
-        for (x = 1; x <= n; x++)
-        {
-        _putchar(95);
-        }
-        _putchar('\n');
-        return;
-        }
+	for (y = 1; y <= n; y++)
+	{
+	for (x = 1; x < y; x++)
+	{
+	_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	_putchar('\n');
+	return;
+	}
