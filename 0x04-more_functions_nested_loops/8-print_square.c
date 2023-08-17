5c74a4ab9835aec31679ac9a3afcaf5fe7e030dc
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-17 16:22:11 +0000

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - to make a square with side equal size
+ * @size : the dimensions of the square
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+	{
+	int x;
+	int y;
+
+	for (x = 1; x <= size; x++)
+	{
+	for (y = 1; y <= size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	return;
+	}
