99b66879a27425ad3b8df392f53df6553f7108f0
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-17 22:53:01 +0000

    task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_number - prints an given integer number
+ * @n: is the number of integers
+ * Return: void
+ */
+
+void print_number(int n);
+{
+int l;
+
+l = n;
+
+if (n < 0)
+{
+_putchar(45)
+l = -l
+}
+
+_putchar(l / 10 + '0');
+_putchar(l % 10 + '0');
+}
+return;
+}
