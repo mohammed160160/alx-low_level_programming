0ac285d77e13aff7f35bc635e92c63437baade44
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-17 17:54:39 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - to type Fizz if it's a multiple of 3, Buff if it's a multiple of
+ * main - to type Fizz if it's a multiple of 3, Buzz if it's a multiple of
  * 5 or both if it's multiple of 3 and 5
  * Return: 0
  */
@@ -12,11 +12,11 @@ for (x = 1; x <= 100 ; x++)
 {
 if (x % 3 == 0 && x % 5 == 0)
 {
-printf("FizzBuff");
+printf("FizzBuzz");
 }
 else if (x % 5 == 0)
 {
-printf("Buff");
+printf("Buzz");
 }
 else if (x % 3 == 0)
 {
