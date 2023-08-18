929ecbd14245626668b5e799bf8191b9765eef19
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-18 14:35:24 +0000

    task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,9 @@
 void print_number(int n)
 {
 int x;
+unsigned int y;
 
 x = 1000000000;
-
-if (n < 0)
-{
-_putchar(45);
-n = -n;
-}
 
 if (n == 0)
 {
@@ -23,15 +18,26 @@ _putchar(0 + '0');
 return;
 }
 
-while (n / x == 0)
+
+if (n < 0)
+{
+_putchar(45);
+y = -n;
+}
+else
+{
+y = n;
+}
+
+while (y / x == 0)
 {
 x = x / 10;
 }
 
 while (x != 1)
 {
-_putchar(n / x + '0');
-n = n % x;
+_putchar(y / x + '0');
+y = y % x;
 x = x / 10;
 }
 
