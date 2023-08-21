8bba66fa56dd28b06358d394c483fcf880624ae9
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-21 20:32:41 +0000

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - prints the value of integer in a string
+ * @s: The string the function is working on
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+int n;
+
+n = atoi(s);
+return (n);
+}
