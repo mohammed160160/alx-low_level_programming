1e4e212e30bf4468c76934215bccbd01a3ba9236
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-21 20:54:39 +0000

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * _atoi - prints the value of integer in a string
@@ -11,8 +9,28 @@
 
 int _atoi(char *s)
 {
-int n;
+int x;
+int si;
+int v;
 
-n = atoi(s);
-return (n);
+x = 0;
+si = 1;
+v = 0;
+
+while (s[x] != '\0' && (s[x] < '0' || s[x] > '9'))
+{
+if (s[x] == '-')
+{
+si = si * -1;
+}
+x++;
+}
+
+while (s[x] != '\0' && s[x] >= '0' && s[x] <= '9')
+{
+v = v * 10 + si * (s[x] - '0');
+x++;
+}
+
+return (v);
 }
