8549a22d6677aebebd7ad0298d349a96b0dfa9aa
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-22 20:31:22 +0000

    task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,22 +9,26 @@
 
 int main(void)
 {
-	int pw[64], i, s = 0, n;
+int pw[64];
+int i;
+int s = 0;
+int n;
 
-	srand(time(NULL));
+srand(time(NULL));
+
 	for (i = 0; i < 64; i++)
 	{
-		pw[i] = rand() % 78;
-		s += pw[i] + '0';
-		putchar(pw[i] + '0');
-
+	pw[i] = rand() % 78;
+	s += pw[i] + '0';
+	putchar(pw[i] + '0');
+	}
 		if ((2772 - s) - '0' < 78)
 		{
-			n = 2772 - s - '0';
-			s += n;
-			putchar(n + '0');
-			break;
+		n = 2772 - s - '0';
+		s += n;
+		putchar(n + '0');
+		break;
 		}
-	}
-	return (0);
+}
+return (0);
 }
