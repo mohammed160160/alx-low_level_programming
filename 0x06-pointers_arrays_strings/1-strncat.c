3950e866853254dcd0a4032a04173af115080ef5
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-23 17:51:38 +0000

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,23 +2,32 @@
 #include <stdio.h>
 
 /**
- * *_strncat - updating the value of a pointer to 98
- * @dest: The pointer that is used
- * @src:
- * @n:
- * Return: Null
+ * *_strncat - concatenates 2 string but n segment of the source
+ * @dest: The string that the is added on by the source
+ * @src: The string that a segment used for string
+ * @n: The segment of src that is used
+ * Return: the pointer to dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
+int l;
+int x;
 
+l = 0;
 
+for (x = 0; dest[x] != '\0'; x++)
+{
+l++;
+}
 
+x = 0;
 
+while (src[x] != '\0' && x < n)
+{
+dest[l + x] = src[x];
+x++;
+}
 
-
-
-
-
-
+return (dest);
 }
