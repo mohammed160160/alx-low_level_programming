850b162dd16a6a7137ec5d5b1154a2741ba37872
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-24 13:42:27 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,23 +3,32 @@
 
 /**
  * *cap_string - Turns the start of all words in a sentence into capital
- * @'': The pointer that is used
+ * @str: The pointer that is used
  * Return: The pointer used
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 int x;
 
 x = 0;
 
-while (''[x] != '\0')
+while (str[x] != '\0')
 {
-        if (''[x] >= 'a' && ''[x] <= 'z')
-        {
-        ''[x] -= 32;
-        x++;
-        }
+if (str[x] == ' ' || str[x] == '\n' || str[x] == '\t' || str[x] == ',' ||
+str[x] == ';' || str[x] == '.' || str[x] == '!' || str[x] == '?' ||
+str[x] == '"' || str[x] == '(' || str[x] == ')' || str[x] == '{' ||
+str[x] == '}')
+
+{
+
+if (str[x + 1] >= 'a' && str[x + 1] <= 'z')
+{
+str[x + 1] -= 32;
 }
-return ('');
+
+}
+x++;
+}
+return (str);
 }
