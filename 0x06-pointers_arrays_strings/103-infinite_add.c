a2598ef28943eadb98590388fa871435c6096a31
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-24 20:25:55 +0000

    task 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,44 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
- * *rot13 - using rot13 encryption on a file
- * @str: The pointer that is used
- * Return: The pointer to the string
+ * *infinite_add - Adds the numbers in 2 arrays
+ * @n1: The first number array
+ * @n2: The second number array
+ * @r: The array that host the total
+ * @size_r: The size of the total array
+ * Return: The value in r which is the total of the operation
  */
 
 
-char *rot13(char *str)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int x;
-int y;
-char alp[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char bet[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int x = 0, l1 = 0, l2 = 0, c = 0, bigl;
 
-x = 0;
-while (str[x] != '\0')
+for (x = 0; n1[x] != '\0'; x++)
 {
-        for (y = 0; y < 52; y++)
-        {
-                if (str[x] == alp[y])
-                {
-                        str[x] = bet[y];
-                        break;
-                }
-        }
-x++;
+l1++;
 }
-return (str);
-
-
-
-
-int l;
-int x;
-
-l = 0;
-
-
-for (x = 0; dest[x] != '\0'; x++)
+for (x = 0; n2[x] != '\0'; x++)
 {
-l++;
+l2++;
 }
 
-x = 0;
+if (l1 >= l2)
+{
+bigl = l1;
+}
+else
+{
+bigl = l2;
+}
+
+if (size_r < bigl)
+{
+return (0);
+}
+
+x = bigl;
+
+while (x != 0)
+{
+r[x] = r [x - 1] + r[x - 1] + c;
+if (r[x] >= 10 && x != 1)
+{
+r[x] -= 10;
+c = 1;
+}
+if (r[x] < 10 && x != 1)
+{
+c = 0;
+}
+x--;
+}
+return (r);
+}
