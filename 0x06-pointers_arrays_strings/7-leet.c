6034b496c5b4f93948945f3ae37723edbad77a8d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-24 15:26:24 +0000

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,18 +2,30 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - updating the value of a pointer to 98
- * @n: The pointer that is used
- * Return: Null
+ * *leet - transform some letters to leet language
+ * @str: The pointer that is used
+ * Return: The pointer to the string
  */
 
 
-char *leet(char *)
+char *leet(char *str)
 {
+int x;
+int y;
+char le[10] = "aAeEoOtTlL";
+char et[5] = "43071";
 
-
-
-
-
-
+x = 0;
+while (str[x] != '\0')
+{
+	for (y = 0; y < 10; y++)
+	{
+	if (str[x] == le[y])
+		{
+		str[x] = et[y / 2];
+		}
+	}
+x++;
+}
+return (str);
 }
