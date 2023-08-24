2720a82a2fd788c324233dfbe6d80e5b3060ff4d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-24 21:01:05 +0000

    task 103

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints the buffer of a string
+ * @size:
+ * Return: The value in r which is the total of the operation
+ */
+
+void print_buffer(char *b, int size)
+{
+
+
+
+}	
