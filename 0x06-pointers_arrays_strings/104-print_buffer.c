c8b5e38a2eeb8a742fcfe961e0b979b84d028af7
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-25 11:35:14 +0000

    task 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,13 +3,49 @@
 
 /**
  * print_buffer - prints the buffer of a string
- * @size:
+ * @size: The size of the buffer
+ * @b: The string
  * Return: The value in r which is the total of the operation
  */
 
 void print_buffer(char *b, int size)
 {
-
-
-
-}	
+int x = 0, y = 0, z = 0;
+while (x < size)
+{
+printf("%.7x: ", x);
+/*-----------------------------------------*/
+for (y = 0; y < 5; y++)
+{
+if ((y * 2) + x >= size)
+{
+printf("    ");
+}
+else
+{
+printf("%.2x%.2x", b[(y * 2) + x], b[(y * 2) + x + 1]);
+}
+if (y != 4)
+{
+printf(" ");
+}
+}
+printf(" ");
+/*-----------------------------------------------------*/
+for (z = 0; z < 10; z++)
+{
+if (z + x >= size)
+{
+printf(" ");
+}
+else
+{
+printf("%c", b[z + x]);
+}
+}
+/*------------------------------------------------------*/
+printf("\n");
+x = x + 10;
+}
+return;
+}
