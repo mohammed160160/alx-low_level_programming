f371e788db2fd5773130a775c63afa274863b261
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-25 14:55:53 +0000

    task 101

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,7 +11,7 @@
 void print_buffer(char *b, int size)
 {
 int x = 0, y = 0, z = 0;
-while (x <= size)
+while (x < size)
 {
 printf("%.8x: ", x);/*start of first part*/
 for (y = 0; y < 5; y++)/*start of the second part*/
