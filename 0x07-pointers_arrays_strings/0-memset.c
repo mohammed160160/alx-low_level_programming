1059a89a6124016c42c51c02e555037f862bcaf4
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-28 11:17:11 +0000

    task 2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,10 +3,10 @@
 
 /**
  * *_memset - Replaces n length of an array s with the char b
- * @s:The pointer to the main array
- * @n:The number of elements string that get replaced with b
- * @b:The characters used instead of n size
- * Return: a pointer to the memory area s
+ * @s: The pointer to the main array
+ * @n: The number of elements string that get replaced with b
+ * @b: The characters used instead of n size
+ * Return: A pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - Searches for the first occurance of an element c in
+ * the string and prints out everything after it
+ * @s: The pointer to the string
+ * @c: The character being searched in the string
+ * Return: A pointer to the memory area s
+ */
+
+char *_strchr(char *s, char c)
+{
+int x = 0;
+int y;
+
+
+while (s[l] != '\0')
+{
+l++
+}
+
+while (s[x] != '\0')
+{
+	if (s[x] == c)
+	{
+	break;
+	}
+x++;
+}
+
+
+if (x != l)
+{
+for (x; x <= y; x++)
+{
+
+
+
+
+
+
+return
+}
+else
+{
+return ('\0');
+}
+
+}
