5af002aeeb2c7f2d08987388d7726e77025c696e
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-28 10:26:48 +0000

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memset - Replaces n length of an array s with the char b
+ * @s:The pointer to the main array
+ * @n:The number of elements string that get replaced with b
+ * @b:The characters used instead of n size
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int x;
+
+for (x = 0 ; x < n; x++)
+{
+s[x] = b;
+}
+
+return (s);
+}
