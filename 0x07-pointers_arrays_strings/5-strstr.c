9b5d5fe663bd308c78f0edd3740b361a81181992
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-28 20:10:07 +0000

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,29 +2,41 @@
 #include <stdio.h>
 
 /**
- * *_strpbrk - Searches for the first occurance of a string of line 
+ * *_strpbrk - Searches for the first occurance of a string of line
  * pointed to by needle in the string and prints out everything after it
  * @haystack: The pointer to the string
  * @needle: The sub string to be searched in the string
  * Return: A pointer to the memory area s
  */
 
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 {
-int x = 0;
-int y = 0;
+int x = 0, l = 0, cc = 0;
+
+while (needle[l] != '\0')
+{
+l++;
+}
 
 while (haystack[x] != '\0')
 {
-	for (y = 0; accept[y] != '\0';  y++)
+
+	if (haystack[x] == needle[cc])
 	{
-		if (haystack[x] != needle[y])
-		{
-		break;
-		}
-		return (s + x);
+	cc++;
 	}
+	else
+	{
+	cc = 0;
+	}
+
+if (cc == l)
+{
+return (haystack + x - cc + 1);
+}
+
 x++;
 }
+
 return ('\0');
 }
