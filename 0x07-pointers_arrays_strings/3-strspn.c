8bdc5e66a4b38105ea1852bab5b5cb7098a65c39
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-28 18:44:20 +0000

    task 1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - counts from the start of the string (s) every occurance of
+ * the bytes in the accept string until a value not in accept occur
+ * @s: The pointer to the string
+ * @accept: The characters the string count
+ * Return: The value of (s) that contain value in accept before a non
+ * accept value appear
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int x = 0;
+int y = 0;
+
+while (s[x] != '\0')
+{
+	for (y = 0; accept[y] != '\0';  y++)
+	{
+		if (s[x] == accept[y])
+		{
+		return (s + x);
+		}
+	}
+x++;
+}
+return ('\0');
+}
