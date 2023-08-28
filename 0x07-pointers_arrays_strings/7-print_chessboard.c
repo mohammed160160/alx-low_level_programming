7b9e9f8e8a45f5840fb9ae69ec8a73a3bfa2e9a5
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-28 17:34:08 +0000

    task 8

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Write a function that prints the chessboard
+ * @a: The pointer to the array of chess
+ * Return: Null
+ */
+void print_chessboard(char (*a)[8])
+{
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Write a function that prints both diagonal sides
+ * of a 2d array
+ * @a: The pointer to the array in the shape of a square
+ * @size: The size of the array
+ * Return: Null
+ */
+void print_diagsums(int *a, int size)
+{
+int x;
+int dia1;
+int dia2;
+
+for (x = 0; x < size; x++)
+{
+dia1 += a[(size * x) + x];
+dia2 += a[(size * x) + (size - x) - 1];
+}
+
+printf("%i, %i\n", dia1, dia2);
+
+}
