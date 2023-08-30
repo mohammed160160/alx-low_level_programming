c402c5ef7e9db19be15575bf33e9e2f02a4d3984
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-30 21:02:44 +0000

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _puts_rev_recursion - Prints out the string pointed to by s in reverse
+ * _print_rev_recursion - Prints out the string pointed to by s in reverse
  * @s: Pointer to the string
  * Return: Always 0.
  */
@@ -11,17 +11,36 @@ void _print_rev_recursion(char *s)
 
 {
 
-if (*s == '\0')
-{
-_putchar('\n');
+
+
 }
 
-if (*s != '\0')
+/**
+ * lengthforrev - finds the length of the string
+ * @l: The length of the string
+ * @s: Pointer to the string
+ * Return: Always 0.
+ */
+
+int lengthforrev(char *s, int l)
+
 {
-_putchar(*s);
-s++;
-_print_rev_recursion(s);
+
+
+
 }
+
+/**
+ * printingrev - Prints out the string in reverse
+ * @l: The length of the string
+ * @s: Pointer to the string
+ * Return: Always 0.
+ */
+
+void printingrev(char *s, int l)
+
+{
+
 
 
 }
