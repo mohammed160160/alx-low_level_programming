3015cf8ae9498f65fbab2538ee51e6274f7520a7
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-30 22:52:29 +0000

    task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,16 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * wildcmp - checks if 2 strings are identical and one of them having
+ * special symbols
+ * @s1:The pointer to the string without special cases
+ * @s2:The pointer to the special case string
  * Return: Always 0.
  */
 
 int wildcmp(char *s1, char *s2)
 {
-
-
-
-
-
+return (0);
 }
