967f60f4392fb19d082702b7032e81d710d7bf44
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-30 15:22:37 +0000

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,9 +2,25 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _pow_recursion - This function is similar to the (pow) function which gives
+ * the number (x) to the power of (y)
+ * @x: The number that is applied to
+ * @y: The power the number is multiplied by
+ * Return: -1 if y less than 0 and the power if more
  */
 
 int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+{
+return (-1);
+}
+
+if (y == 0)
+{
+return (1);
+}
+
+return (x * _pow_recursion(x, y - 1));
+}
