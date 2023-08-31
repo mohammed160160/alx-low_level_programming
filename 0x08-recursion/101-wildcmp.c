98b6c8f1c9938934dec1bcacf2e712d36553bb0b
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-08-31 20:40:15 +0000

    task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,8 +11,37 @@
 
 int wildcmp(char *s1, char *s2)
 {
+if (*s2 == '*' && *(s2 + 1) == '\0')
+{
+return (1);
+}
 
-*s1 = *s2;
+if (*s1 == '\0')
+{
+	if (*s2 == '\0')
+	{ return (1); }
+	else
+	{ return (0); }
+}
+if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
 
+if (*s2 == '*' && *(s2 + 1) == '*')
+	{
+	return (wildcmp(s1, s2 + 1));
+	}
+if (*s2 == '*' && *(s2 + 1) != '*')
+	{
+	if (*s1 != *(s2 + 1))
+	{
+	return (wildcmp(s1 + 1, s2));
+	}
+	else
+	{
+	return (wildcmp(s1, s2 + 1));
+	}
+	}
 return (0);
 }
