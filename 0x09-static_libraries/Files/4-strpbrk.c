447018fd0e3cee56d034b6f68aaa637a64e3f072
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-01 12:03:35 +0000

    task 0

---
+++ 0x09-static_libraries/Files/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - Searches for the first occurance of any byte in the character
+ * pointed to by accept in the string and prints out everything after it
+ * @s: The pointer to the string
+ * @accept: The set of bytes wanted to be searched
+ * Return: A pointer to the memory area s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int x = 0;
+int y = 0;
+
+while (s[x] != '\0')
+{
+	for (y = 0; accept[y] != '\0';  y++)
+	{
+		if (s[x] == accept[y])
+		{
+		return (s + x);
+		}
+	}
+x++;
+}
+return ('\0');
+}
