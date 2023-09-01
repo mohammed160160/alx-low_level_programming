bb0944751c4bca6a94c0df4a23f635fce076c186
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-01 18:45:14 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,31 +5,42 @@
  * main - adds the value of the agruments together
  * @argc: The argument counter
  * @argv: The argument vector(array)
- * Return: 1 if there a non int value in the argument and 0 if successful
+ * Return: 1 if there a non digit value in the argument and 0 if successful
  */
 
 int main(int argc, char *argv[])
 {
 int x = 0;
 int y = 0;
+int z = 0;
+char *c;
 
-if (argc == 1)
+if (argc == 1) /*checks if no arguments is put*/
 {
-printf("0");
+printf("0\n");
 return (0);
 }
 
-if (argc != 3)
+for (x = 1; x < argc; x++)/*checks if all characters are digits*/
 {
-printf("Error\n");
-return (1);
+c = argv[x];
+
+	for (z = 0; c[z] != '\0'; z++)
+	{
+		if (c[z] > 57 || c[z] < 48)
+		{
+		printf("Error\n");
+		return (1);
+		}
+	}
+
 }
 
-
-for (x = 0; x < argc; x++)
+for (x = 0; x < argc; x++)/*Calculates the total*/
 {
-y += atoi(argv[x])
+y += atoi(argv[x]);
 }
 
-printf("%i\n", y);
+printf("%i\n", y); /*prints the result*/
+return (0);
 }
