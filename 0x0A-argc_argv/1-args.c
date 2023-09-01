54426f7458dab1b6d2864ba4d42ad667ccafdbd9
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-01 15:56:56 +0000

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: Success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-printf("%s", argv[0]);
+printf("%i\n", (argc - 1));
 return (0);
 }
