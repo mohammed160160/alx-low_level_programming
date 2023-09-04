c093166ac4422de6f31a513b4d71a464eb626fd7
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-04 12:24:34 +0000

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,13 @@ char *create_array(unsigned int size, char c)
 char *x;
 unsigned int y;
 
-x = malloc(size);
 
-if (size == 0)
+x = malloc(size * sizeof(char));
+
+if (x == NULL)
 {
 return (NULL);
 }
-
-x = malloc(size * sizeof(char));
 
 for (y = 0; y < size; y++)
 {
