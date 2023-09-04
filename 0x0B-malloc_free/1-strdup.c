35f03e4ed11ee6f3a3907971eade9b65711b8467
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-04 12:49:58 +0000

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,5 +10,31 @@
 
 char *_strdup(char *str)
 {
+char *x;
+int y;  /*The counter*/
+int l = 0;  /*length of str*/
 
+while(str[l] != '\0')
+{
+l++;
+}
+
+if (l == 0)
+{
+return (NULL);
+}
+
+x = malloc((l + 1) * sizeof(char));
+
+if (x == NULL)
+{
+return (NULL);
+}
+
+for (y = 0; y < l; y++)
+{
+x[y] = str[y];
+}
+
+return (x);
 }
