49cf771cd4aa4dd1dba23a717d2aa23b63fb884a
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-04 17:10:04 +0000

    task 100

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 
 /**
  * free_grid - To free an 2d array from memory
- * @*grid:The grid wants to be free
+ * @grid:The grid wants to be free
  * @height:The height of the array
  * Return: Nothing.
  */
