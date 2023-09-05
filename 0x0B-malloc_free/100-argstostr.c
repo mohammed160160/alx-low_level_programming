a288b52f2b19ab08d4b14071bb2b8ab0116ee1d0
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-05 07:57:19 +0000

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@
 char *argstostr(int ac, char **av)
 {
 char *s;
-int tleng, cleng, l, x, y;/*tleng= Totallength - cleng current - l(length of the current array) - xyz variables*/
+int tleng, cleng, l, x, y;
 
 if (ac == 0 || av == NULL)
 {
@@ -21,14 +21,14 @@ return (NULL);
 for(x = 0; x < ac; x++) /*Total length of the compiler*/
 {
 l = 0;
-while (av[x][l] != '\0')
-{
-l++;
-}
+while (*av[l] != '\0')
+	{
+	l++;
+	}
 tleng += l;
 }
 
-s = malloc(tleng + 1 * sizeof(char)); /*Declaring the memory*/
+s = malloc((tleng + 1) * sizeof(char)); /*Declaring the memory*/
 if (s == NULL)
 {
 return (NULL);
