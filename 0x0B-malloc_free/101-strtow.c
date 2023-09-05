d933e922d262ad340c2e8b8fa5dbf23b46144536
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-05 19:43:47 +0000

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -20,7 +20,7 @@ word = word_counter(str, los);
 printf("The number of elements and words in this string  %i--%i\n", los, word);
 if (word == 0)
 { return (NULL); }
-s = malloc(word * sizeof(char*));/*declaring the main array*/
+s = malloc((word - 1) * sizeof(char*));/*declaring the main array*/
 if (s == NULL)
 { return (NULL); }
 for (a = 0; a < los; a++)/*declaring the sub array*/
@@ -77,15 +77,16 @@ return (x);
 int word_counter(char *str, int q)
 {
 int x;
+int y = 0;
 
-for (x = 0; x < q; x++)
+for (x = 1; x < q; x++)
 {
 	if ((str[x - 1] == 32 || str[x - 1] == '\0') && str[x] != 32)
 	{
-	q++;
+	y++;
 	}
 }
-return (q);
+return (y);
 }
 
 /**
