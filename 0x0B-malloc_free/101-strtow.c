e1e42c6ef2fa5c2109d035af152e2e49fc4e8dff
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-05 14:15:33 +0000

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,1 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - To free an 2d array from memory
+ * @grid:The grid wants to be free
+ * @height:The height of the array
+ * Return: Nothing.
+ */
+
 char **strtow(char *str);
+{
+char **s;
+if (str == 0 || str == NULL)
+{
+return (NULL);
+}
+
+
+return (s)
+}
