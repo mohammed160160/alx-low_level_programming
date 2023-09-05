5291a57ba91e51a56c3c39b184399d7c14d4e8f8
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-05 15:10:44 +0000

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,21 +3,65 @@
 #include <stdlib.h>
 
 /**
- * free_grid - To free an 2d array from memory
- * @grid:The grid wants to be free
- * @height:The height of the array
- * Return: Nothing.
+ * **strtow - prints out each word in a string
+ * @str:The string which it needs to be searched
+ * Return: Null if str is empty or cannot allocate space 1 if else
  */
 
 char **strtow(char *str)
 {
 char **s;
+int now = 0;
+int los = 0;
 
 if (str == '\0' || str == NULL)
 {
 return (NULL);
 }
 
+los = string_length(str);
+now = word_counter(str, los);
+
+
+
+
+
 
 return (s);
+}
+/**
+ * string_length - finds the length of the string
+ * @str: The string which it needs to be searched
+ * Return: The length of the string
+ */
+int string_length(char *str)
+{
+int x;
+
+while (x != '\0')
+{
+x++
+}
+
+return (x);
+}
+/**
+ * word_counter - finds the number of words in a string
+ * @str: The string which it needs to be searched
+ * @los: Length of String
+ * Return: The number of words in a string
+ */
+int word_counter(char *str, int los)
+{
+int x;
+int count;
+
+for (x = 0; x < los; x++)
+{
+	if (str[x] == 32 && str[x++] != 32 && str[x++] != '\0')
+	{
+	count++;
+	}
+}
+return (count);
 }
