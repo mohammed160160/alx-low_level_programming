e7342baef237fe7dadeef49d378d23e1f26443df
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-07 13:23:47 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,60 +3,45 @@
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * Return: Nothing.
+ * *string_nconcat - Places number of bytes from s2 next to s1 then
+ * the NULL Terminator
+ * @s1: the first string
+ * @s2: the second string
+ * @n: The number of characters in s2 used
+ * Return: The pointer to the combined string or NULL if no space is found
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
 char *x;
 unsigned int y, la = 0, lb = 0;
 
 if (s1 == NULL)
-{
-	s1 = "";
-}
+{ s1 = ""; }
 if (s2 == NULL)
-{
-	s2 = "";
-}
+{ s2 = ""; }
 
 while (s1[la] != '\0')
-	{
-	la++;
-	}
+{ la++; }
 while (s2[lb] != '\0')
-	{
-	lb++;
-	}
-
+{ lb++; }
 
 if (n >= lb)
-{
-n = lb;
-}
+{ n = lb; }
 
-
-x = malloc((la + n + 1) * sizeof(char));
+x = malloc((la + n + 2) * sizeof(char));
 
 if (x == NULL)
 {
-return (NULL);
+	return (NULL);
 }
 
 for (y = 0; y < la + n; y++)
 {
 	if (y < la)
-{
-	x[y] = s1[y];
-}
+	{ x[y] = s1[y]; }
 	else
-	{
-	x[y] = s2[y - la];
-	}
+	{ x[y] = s2[y - la]; }
 }
 
 return (x);
