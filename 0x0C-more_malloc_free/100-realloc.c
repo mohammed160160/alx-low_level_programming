972e077a37f17060c5de4e90bab196d7774251d2
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-07 18:33:43 +0000

    task 100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *array_range - makes an array of integers for min to max values
+ * @min: The min value
+ * @max: The max value
+ * Return: A pointer to the array or NULL if failed
+ */
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+
+
+
+if (old size == new size)
+{ return (ptr); }
+if (ptr != NULL && new size == 0)
+{
+free(ptr)
+return (NULL)
+}
+
+
+
+
+
+
+
+
+return(ptr)
+}
