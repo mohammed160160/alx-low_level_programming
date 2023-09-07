0fbf0c2862da17d9bea203f2cb5d5e23c0b14b03
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-07 13:12:19 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,10 +12,52 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
+char *x;
+unsigned int y, la = 0, lb = 0;
+
+if (s1 == NULL)
+{
+	s1 = "";
+}
+if (s2 == NULL)
+{
+	s2 = "";
+}
+
+while (s1[la] != '\0')
+	{
+	la++;
+	}
+while (s2[lb] != '\0')
+	{
+	lb++;
+	}
 
 
+if (n >= lb)
+{
+n = lb;
+}
 
 
+x = malloc((la + n + 1) * sizeof(char));
 
+if (x == NULL)
+{
+return (NULL);
+}
 
+for (y = 0; y < la + n; y++)
+{
+	if (y < la)
+{
+	x[y] = s1[y];
+}
+	else
+	{
+	x[y] = s2[y - la];
+	}
+}
+
+return (x);
 }
