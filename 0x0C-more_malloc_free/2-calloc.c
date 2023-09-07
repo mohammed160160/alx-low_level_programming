1f7241ac36a5685d7278a983e3e1640a776f5efe
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-07 16:15:17 +0000

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,19 +3,39 @@
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- * Return: Nothing.
+ * *_calloc - This function should work like calloc which allocates memory
+ * space like malloc but inizalizes all values to 0
+ * @nmemb: the number of elements in the array
+ * @size: the size of the elements in the array
+ * Return: pointer to the build array or NULL if a condition failed
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+void *x;
+unsigned int t;
+unsigned int c;
+int* ci;
 
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
 
+t = nmemb * size;
 
+x = malloc(t);
 
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
 
+ci = (int*)x;
+for (c = 0; c < t; c++)
+{
+ci[c] = 0;
+}
 
-
+return (ci);
 }
