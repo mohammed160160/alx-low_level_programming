84a9608baeffb8204607a5a6eee521677f754fa5
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-07 11:22:57 +0000

    starting the tasks

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+
+
+
+
+
+
+}
