cb9c3d9f3c06386b9abb5808c2d7ec4f7d6a7c7f
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-11 20:20:38 +0000

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,15 +2,12 @@
 #include "dog.h"
 
 /**
- * free_dog - This structure initializes a variable in the structure
+ * free_dog - This program frees a structure
  * @d: Writes the owner of the dog
  * Return: A pointer to the structure which contains the information of new dog
  */
 
 void free_dog(dog_t *d)
 {
-free(d->name);
-free(d->age);
-free(d->owner);
 free(d);
 }
