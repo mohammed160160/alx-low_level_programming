e1f315d51264a3b3ff1ead176a50cc995f95b9fa
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-11 18:25:12 +0000

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,13 @@
  * @owner: Writes the owner of the dog
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
