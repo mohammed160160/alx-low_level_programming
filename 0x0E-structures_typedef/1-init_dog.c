b28f6a9486c4f4fb6ad329359f13a2b0dff31028
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-11 14:22:29 +0000

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * init_dog - This structure initializes a variable in the structure
- * @di: The name of the initialized character
+ * @d: The name of the initialized character
  * @name: Writes the name of the dog
  * @age: Writes the age of the dog in float
  * @owner: Writes the owner of the dog
@@ -11,7 +11,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-(*d).name = name;
-(*d).age = age;
-(*d).owner = owner;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
