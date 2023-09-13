ff9f96bb9dff66b43b8d5a055b41b4480401866c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-13 19:37:45 +0000

    task 0

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argv: the argument vector
+ * @argc: The argument counter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+exit(1);
+}
+
+
+return (0);
+}
---
+++ 0x0F-function_pointers/100-x.s
@@ -0,0 +1,64 @@
+	.file	"100-x.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Error"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	cmpl	$2, -4(%rbp)
+	je	.L2
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$1, %edi
+	call	exit@PLT
+.L2:
+	movq	-16(%rbp), %rax
+	addq	$8, %rax
+	movq	(%rax), %rax
+	movq	%rax, %rdi
+	call	atoi@PLT
+	testl	%eax, %eax
+	jns	.L3
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$1, %edi
+	call	exit@PLT
+.L3:
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
