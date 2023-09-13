c3a1b50e902c0ca4fe1136a18576bb2394fd924f
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-13 12:34:54 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+
+/**
+ * print_name - Prints a name(being a bridge between 2 functions and
+ * the main function using function pointers)
+ * @name: The name in question
+ * @f: The style of the writting
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
+
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory is to study how to use pointer to point at functions as bridges or as array of pointers
