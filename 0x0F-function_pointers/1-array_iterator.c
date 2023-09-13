80114fbcef09a9a31d6508dfd453a522ff628f4e
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-13 13:21:25 +0000

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Prints an array with size and formating as action
+ * the main function using function pointers)
+ * @array: An array of integers
+ * @size: The size of the array
+ * @action: The action applied on the array
+ * Return: Nothing but it can fail if (size is less than or equal 0)
+ * , (there isnt an array) or (there isnt an appliable action)
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned long int x = 0;
+
+if (size <= 0)
+{
+return;
+}
+
+if (array == NULL)
+{
+return;
+}
+
+if (action == NULL)
+{
+return;
+}
+
+
+for (x = 0; x < size ; x++)
+{
+action(array[x]);
+}
+
+}
