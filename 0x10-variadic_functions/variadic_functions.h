29ffaea2840cbeafe4492d854bf03f22e6d368f8
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-14 19:57:48 +0000

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include<stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-
 void floatprint(void *x);
 void stringprint(void *x);
 void characterprint(void *x);
 void integerprint(void *x);
-
 
 
 /**
@@ -25,8 +25,5 @@ typedef struct letters
 	char *c;
 	void (*f)(void *);
 } printer;
-
-
-
 
 #endif
