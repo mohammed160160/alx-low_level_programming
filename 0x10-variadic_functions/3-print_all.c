1cc998c2f28ca59fb6325115f1be6cb815e43985
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-14 21:48:06 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,13 +11,10 @@
  */
 void print_all(const char * const format, ...)
 {
-char c;
-char* d;
-float b;
 va_list allvalue;
-int a,left = 0,up = 0, down = 0;
-printer print[] = {{"s", stringprint}, {"c", characterprint}, {"i", integerprint}
-, {"f", floatprint}};
+int left = 0, up = 0, down = 0;
+char print[] = "scif";
+
 va_start(allvalue, format);
 while (format[up] != '\0')
 {
@@ -25,31 +22,31 @@ down = 0;
 left = 0;
 	while (down < 4)
 	{
-		if (print[down].c[0] == format[up])
+	if (print[down] == format[up])
+	{
+		switch (format[up])
 		{
-			switch (format[up])
-			{
-				case 'i': a = va_arg(allvalue, int);
-				print[down].f(&a);
-				left++;
-				break;
-				case 'f': b = va_arg(allvalue, double);
-				print[down].f(&b);
-				left++;
-				break;
-				case 'c': c = va_arg(allvalue, int);
-				print[down].f(&c);
-				left++;
-				break;
-				case 's': d = va_arg(allvalue, char *);
-				print[down].f(d);
-				left++;
-				break;
-			}
-			break;
+		case 'i':
+		printf("%i", va_arg(allvalue, int));
+		left++;
+		break;
+		case 'f':
+		printf("%f", va_arg(allvalue, double));
+		left++;
+		break;
+		case 'c':
+		printf("%c", va_arg(allvalue, int));
+		left++;
+		break;
+		case 's':
+		printf("%s", va_arg(allvalue, char *));
+		left++;
+		break;
 		}
-		down++;
+	break;
 	}
+	down++;
+}
 if (format[up + 1] != '\0' && left == 1)
 { printf(", "); }
 up++;
@@ -57,31 +54,3 @@ up++;
 va_end(allvalue);
 printf("\n");
 }
-/**
- * floatprint - prints a float
- * @x: A void pointer to the location
- * Return: Nothing since it's a print function
- */
-void floatprint(void *x)
-{ printf("%f", *(float *)x); }
-/**
- * stringprint - prints a string
- * @x: A void pointer to the location
- * Return: Nothing since it's a print function
- */
-void stringprint(void *x)
-{ printf("%s", (char *)x); }
-/**
- * characterprint - prints a character
- * @x: A void pointer to the location
- * Return: Nothing since it's a print function
- */
-void characterprint(void *x)
-{ printf("%c", *(char *)x); }
-/**
- * integerprint - prints an integer
- * @x: A void pointer to the location
- * Return: Nothing since it's a print function
- */
-void integerprint(void *x)
-{ printf("%i", *(int *)x); }
