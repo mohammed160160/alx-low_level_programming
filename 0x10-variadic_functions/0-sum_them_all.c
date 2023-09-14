64fecfac08a798b9a56a5cf93d1d52764e82017d
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-14 10:28:30 +0000

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,4 +12,26 @@
  *
  */
 
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int x = 0;
+int y = 0;
+va_list xyz;
+
+if (n == 0)
+{
+return (0);
+}
+
+
+va_start(xyz, n);
+
+for (x = 0; x < n; x++)
+{	
+y += va_arg(xyz, int);
+}
+
+va_end(xyz);
+
+return (y);
+}	
