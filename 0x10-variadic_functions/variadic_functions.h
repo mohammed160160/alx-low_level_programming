6a6bfca9971b387e17c2e4d285c423a8fb4e0ba8
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-14 18:11:18 +0000

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdio.h>
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct op - Struct op
- *
- * @The character: The operator
- * @f: The function associated
- */
 
 
 void floatprint(void *f);
@@ -22,12 +13,16 @@ void characterprint(void *c);
 void integerprint(void *i);
 
 
-
+/**
+ * struct letters - decided depending on the letter which identifier to use
+ * @c: The indentifier
+ * @f: The function associated
+ */
 
 typedef struct letters
 {
-    char *c;
-    void (*f)(void*);
+	char *c;
+	void (*f)(void *);
 } printer;
 
 
