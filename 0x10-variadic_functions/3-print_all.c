016b7eefdbea3aa18e8e08980cbb985e1a797687
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-14 22:10:01 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 {
 va_list allvalue;
 int left = 0, up = 0, down = 0;
-char print[] = "scif";
+char print[] = "scif", *right;
 
 va_start(allvalue, format);
 while (format[up] != '\0')
@@ -22,26 +22,30 @@ down = 0;
 left = 0;
 	while (down < 4)
 	{
-	if (print[down] == format[up])
-	{
-		left++;
-		switch (format[up])
+		if (print[down] == format[up])
 		{
-		case 'i':
-		printf("%i", va_arg(allvalue, int));
-		break;
-		case 'f':
-		printf("%f", va_arg(allvalue, double));
-		break;
-		case 'c':
-		printf("%c", va_arg(allvalue, int));
-		break;
-		case 's':
-		printf("%s", va_arg(allvalue, char *));
+		left++;
+			switch (format[up])
+			{
+			case 'i':
+			printf("%i", va_arg(allvalue, int));
+			break;
+			case 'f':
+			printf("%f", va_arg(allvalue, double));
+			break;
+			case 'c':
+			printf("%c", va_arg(allvalue, int));
+			break;
+			case 's':
+			right = va_arg(allvalue, char *);
+			if (right == NULL)
+			{ printf("(nil)"); }
+			else
+			{ printf("%s", right); }
+			break;
+			}
 		break;
 		}
-	break;
-	}
 	down++;
 }
 if (format[up + 1] != '\0' && left == 1)
