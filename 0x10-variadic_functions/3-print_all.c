eb67e71306c73260dd50ff551345e2a9ff7d9369
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-14 15:59:27 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,15 +1,93 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 #include "variadic_functions.h"
 
 /**
- *
- *
- *
- *
- *
- *
+ * print_all - prints all argument in the format specified
+ * @*: to ensure format doesnt change
+ * @format: The format for each argument
+ * @...: The arguments
+ * Return: Nothing since it's a print function
  */
 
 void print_all(const char * const format, ...)
+{
+void* space;
+va_list allvalue;
+format selection[] = {{"s",, "c", "i", "f"};
+int up = 0;
+
+va_start(allvalue, (strlen(format) + 1));
+
+while(count < (strlen(format) + 1))
+{
+space = va_arg()
+
+if (format[up] == s || format[up] == i || format[up] == f || format[up] == c)
+{
+selection();
+}
+
+count++;
+}
+
+va_end(allvalue);
+printf("\n");
+}
+
+
+/**
+ * print_all - prints all argument in the format specified
+ * @*: to ensure format doesnt change
+ * @format: The format for each argument
+ * @...: The arguments
+ * Return: Nothing since it's a print function
+ */
+
+void floatprint(float f)
+{	
+printf("%f", f)
+}
+
+
+
+/**
+ * print_all - prints all argument in the format specified
+ * @*: to ensure format doesnt change
+ * @format: The format for each argument
+ * @...: The arguments
+ * Return: Nothing since it's a print function
+ */
+
+void stringprint(char *s)
+{
+printf("%s", s)
+}
+
+/**
+ * print_all - prints all argument in the format specified
+ * @*: to ensure format doesnt change
+ * @format: The format for each argument
+ * @...: The arguments
+ * Return: Nothing since it's a print function
+ */
+
+void characterprint(char c)
+{
+printf("%c", c)
+}
+
+/**
+ * print_all - prints all argument in the format specified
+ * @*: to ensure format doesnt change
+ * @format: The format for each argument
+ * @...: The arguments
+ * Return: Nothing since it's a print function
+ */
+
+void integerprint(int i)
+{
+printf("%i", i)
+}	
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,20 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct op - Struct op
+ *
+ * @The character: The operator
+ * @f: The function associated
+ */
+typedef struct letters
+{
+    char *c;
+    void (*f)(void);
+} printer;
+
+
+
+
+
 #endif
