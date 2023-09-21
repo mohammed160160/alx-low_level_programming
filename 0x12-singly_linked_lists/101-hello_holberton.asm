96191804eecc0d1b7215681ce4d977b44524a422
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-21 16:52:34 +0000

    task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,14 @@
 section .data
 message db 'Hello, Holberton', 10, 0
-
 section .text
 	extern printf
 	global main
-
 main:
 	mov rdi,message
 	xor rax,rax
 
 	call printf
 
+	mov eax, 60
 	xor edi, edi
 	syscall
