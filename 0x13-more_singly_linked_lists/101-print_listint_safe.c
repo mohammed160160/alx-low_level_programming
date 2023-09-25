360d55cffcc9b0b36c5d90974f739937d08a7f20
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 19:47:47 +0000

    task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h:A pointer to the first element of the list
+ * Return: The number of elements in the linked list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+unsigned int length = 0;
+
+for (length = 0; head != NULL ; length++)
+{
+printf("[%p] %i\n",(void *)head, head->n);
+head = head->next;
+}
+
+return (length);
+}
