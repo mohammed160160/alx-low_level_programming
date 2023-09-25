42fe09e3d14f135b0cad42acef0db6bfe695da0f
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 16:06:12 +0000

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,4 +6,26 @@
  * Return: The number of elements in the linked list
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int x = 0;
+
+if (head == NULL)
+{
+return (NULL);
+}
+
+for (x = 0; x < index; x++)
+{
+	if (head == NULL)
+	{
+	return(NULL);
+	}
+
+head = head->next;
+}
+
+return(head);
+
+
+}
