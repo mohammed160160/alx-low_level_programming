a48777014959792c69eebef98a452831546ba48c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 08:34:50 +0000

    task 0 and 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,3 +5,16 @@
  * @h:A pointer to the first element of the list
  * Return: The number of elements in the linked list
  */
+
+size_t print_listint(const listint_t *h)
+{
+unsigned int length = 0;
+
+for (length = 0; h != NULL ; length++)
+{
+printf("%i\n", h->n);
+h = h->next;
+}
+
+return (length);
+}
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,3 +5,15 @@
  * @h:A pointer to the first element of the list
  * Return: The number of elements in the linked list
  */
+
+size_t listint_len(const listint_t *h)
+{
+unsigned int length = 0;
+
+for (length = 0; h != NULL ; length++)
+{
+h = h->next;
+}
+
+return (length);
+}
