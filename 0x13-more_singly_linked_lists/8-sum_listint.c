0df93af5f5fdf082c19413bafe3ca81b48f39d21
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 15:21:13 +0000

    task

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,9 +1,26 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h:A pointer to the first element of the list
- * Return: The number of elements in the linked list
+ * sum_listint - calculates the sum of a linked list
+ * @head:A pointer to the first element of the list
+ * Return: The sum of the linked list elements
  */
 
-int sum_listint(listint_t *head);
+int sum_listint(listint_t *head)
+{
+int x = 0;
+
+if (head == NULL)
+{
+return (0);
+}
+
+while (head != NULL)
+{
+x += head->n;
+head = head->next;
+}
+
+return (x);
+
+}
