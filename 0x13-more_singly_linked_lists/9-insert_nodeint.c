edbf55e590a5a51d895c9945942e0cd0815732c9
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 19:26:35 +0000

    task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,15 +1,50 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h:A pointer to the first element of the list
- * Return: The number of elements in the linked list
+ * insert_nodeint_at_index - inserts the node at index of
+ * a listint_t linked list.
+ * @idx: The index where the node inserted
+ * @head:A pointer to the first element of the list
+ * @n: The data that is in the new node
+ * Return: The location of the new index or NULL if failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+listint_t *inserted, *I, *J;
+unsigned int x;
 
+if (*head == NULL && idx != 0)
+{ return (NULL); }
 
+inserted = malloc(sizeof(listint_t));
+if (inserted == NULL)
+{ return(NULL); }
 
+inserted->n = n;
+
+J = *head;
+
+if (idx == 0)
+{
+inserted->next = *head;
+*head = inserted;
+return (inserted);
+}
+
+for (x = 0; x < idx - 1; x++)
+{
+if (J == NULL)
+{
+free(inserted);
+return (NULL);
+}
+J = J->next;
+}
+
+I = J->next;
+J->next = inserted;
+inserted->next = I;
+return (inserted);
 
 }
