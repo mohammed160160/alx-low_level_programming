0e17eeb3637d402149a47ee142666a97560d6bc0
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 10:07:13 +0000

    task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,27 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h:A pointer to the first element of the list
- * Return: The number of elements in the linked list
+ * free_listint - frees a linked list of listint_t.
+ * @head:A pointer to the first structure of the list.
+ * Return: Nothing since it frees a structure
  */
+
+void free_listint(listint_t *head)
+{
+listint_t *H;
+
+if (head == NULL)
+{
+return;
+}
+
+while (head->next != NULL)
+{
+H = head->next;
+free(head);
+head = H;
+}
+
+free(head);
+
+}
