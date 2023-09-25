db737d1e2379e27c10353c16f4db72f50b3bd53f
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 08:11:10 +0000

    starting

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h:A pointer to the first element of the list
+ * Return: The number of elements in the linked list
+ */
