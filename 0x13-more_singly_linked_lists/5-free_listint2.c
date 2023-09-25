f28dc585de61f0e8a49944ee7e7a13dd5ce49c56
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 17:49:20 +0000

    task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,8 +10,6 @@ void free_listint2(listint_t **head)
 {
 listint_t *H;
 
-H = malloc(sizeof(listint_t));
-
 if (*head == NULL)
 {
 return;
@@ -25,6 +23,5 @@ free(*head);
 }
 
 free(*head);
-free(H);
 
 }
