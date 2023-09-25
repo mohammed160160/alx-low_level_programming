dd38c3b625efa5504857dcf8ecb888993c4c95df
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-25 21:44:24 +0000

    t

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - insert
+ * @head:A pointer to the first element of the list
+ * Return: The location of the new index or NULL if failed
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *I;
+listint_t *J;
+
+
+if (*head == NULL || head == NULL)
+{
+return (NULL);
+}
+
+
+(*head)->next
+
+I = *head;
+J = NULL;
+
+while(head != NULL)
+{
+
+}
+
+return (*head);
+}
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,11 +9,16 @@
 size_t print_listint_safe(const listint_t *head)
 {
 unsigned int length = 0;
+const listint_t *X = head;
 
 for (length = 0; head != NULL ; length++)
 {
 printf("[%p] %i\n",(void *)head, head->n);
 head = head->next;
+	if (X == head)
+	{
+	break;
+	}
 }
 
 return (length);
