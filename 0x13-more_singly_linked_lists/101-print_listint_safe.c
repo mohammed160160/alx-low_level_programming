0a52a1f8fa4573e28cd025215d820f3b70fc92b3
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-26 15:21:08 +0000

    task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,24 +10,23 @@ size_t print_listint_safe(const listint_t *head)
 {
 unsigned int length = 0;
 const listint_t *Z = head;
-const listint_t *Y = head;
 const listint_t *X = head;
 
 for (length = 0; head != NULL ; length++)
 {
 X = Z;
-		while (X != Y)
+		while (head != X)
 		{
 		X = X->next;
-			if ((X->next == Y->next) && (X != Y))
+			if (head != X && X->next == head->next)
 			{
+			head = head->next;
 			printf("-> [%p] %i\n", (void *)head, head->n);
 			return (length);
 			}
 		}
 
 printf("[%p] %i\n", (void *)head, head->n);
-Y = head;
 head = head->next;
 }
 
