77c4aef4cc231cd89f0a094eeb83227cead6e3d2
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-26 15:08:32 +0000

    task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * print_listint_safe - prints all the elements of a listint_t list
  * @h:A pointer to the first element of the list
  * Return: The number of elements in the linked list
  */
@@ -9,16 +9,32 @@
 size_t print_listint_safe(const listint_t *head)
 {
 unsigned int length = 0;
+const listint_t *Z = head;
+const listint_t *Y = head;
 const listint_t *X = head;
+
+
+if (head == NULL)
+{
+exit(98);
+}
 
 for (length = 0; head != NULL ; length++)
 {
-printf("[%p] %i\n",(void *)head, head->n);
+X = Z;
+		while (X != Y)
+		{
+		X = X->next;
+			if ((X->next == Y->next) && (X != Y))
+			{
+			printf("-> [%p] %i\n", (void *)head, head->n);
+			return (length);
+			}
+		}
+
+printf("[%p] %i\n", (void *)head, head->n);
+Y = head;
 head = head->next;
-	if (X == head)
-	{
-	break;
-	}
 }
 
 return (length);
