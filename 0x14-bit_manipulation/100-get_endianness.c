e3831bf929e23acdb2feb8b26b75fb6e2ade63fd
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-28 15:59:04 +0000

    task 101

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * set_bit - checks the endianness.
+ * @void: The function direction
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+
+
+int bit = 1;
+
+char *c = (char*)&bit;
+
+
+if (*c == 0)
+{
+return (1);
+}
+
+else
+{
+return(0);
+}
+
+
+}
