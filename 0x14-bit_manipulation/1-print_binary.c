c8516bf58772222863206c4b5111741b8649d84c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-28 12:34:20 +0000

    tasks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,17 +8,33 @@
 
 void print_binary(unsigned long int n)
 {
+unsigned long int X = 1;
+int y = 0;
+
+X = X << (sizeof(n)*8 - 1);
+
+if (n == 0)
+{
+_putchar('0');
+return;
+}
 
 
+while (X != 0)
+{
 
+if ((n & X) == 0 && y == 1)
+{	
+_putchar('0');
+}
+	
+	else if ((n & X) == 1)	
+	{
+	_putchar('1');
+	y = 1;
+	}
 
-
-
-
-
-
-
-
-
+X = X >> 1;
+}	
 
 }
