e00f42cea173c1299a873c3be7454490cf39b8e6
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-28 11:13:56 +0000

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,13 +3,15 @@
 
 /**
  * binary_to_uint - check the code
- * @b: 
- * Return: Always 0.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * if b is NULL
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-long unsigned int x = 1;
+unsigned long int x = 1;
 unsigned int y = 0;
 unsigned int z = 0;
 unsigned int value = 0;
@@ -21,14 +23,14 @@ if (*b == '\0')
 while (b[y] != '\0')
 { y++; }
 
-for(z = 1; y >= z; z++)
+for (z = 1; y >= z; z++)
 {
 
-	if(b[y - z] != '0' && b[y-z] != '1')
+	if (b[y - z] != '0' && b[y - z] != '1')
 	{ return (0); }
 
-	if(b[y-z] == '1')
-	{value += x ;}
+	if (b[y - z] == '1')
+	{ value += x; }
 
 	x *= 2;
 }
