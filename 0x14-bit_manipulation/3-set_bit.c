344c22361a9ed446938d7510815fca0db54be24b
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-28 10:31:16 +0000

    task 101

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
-
 /**
  * set_bit - check the code
  * @n:
  * @index:
  * Return: Always 0.
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int bit = 1;
+unsigned int x = 0;
+int byte = 0;
+
+for (x = 0;x != index;x++)
+{
+bit = bit << 1;
+}
+
+
+byte = *n | bit;
+
+return(byte);
+}
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,3 +9,20 @@
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int bit = 1;
+unsigned long int bat = 0;
+unsigned int x = 0;
+int byte = 0;
+
+for (x = 0;x != index;x++)
+{
+bit = bit << 1;
+}
+
+bat = *n | bit;
+
+byte = bat ^ bit;
+
+return(byte);
+}
