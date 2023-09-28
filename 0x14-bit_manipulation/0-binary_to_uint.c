fbb43a925f6fc23fe45b1d661a051c951d284005
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-28 11:05:55 +0000

    tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+long unsigned int x = 1;
+unsigned int y = 0;
+unsigned int z = 0;
+unsigned int value = 0;
+
+if (*b == '\0')
+{ return (0); }
 
 
+while (b[y] != '\0')
+{ y++; }
 
+for(z = 1; y >= z; z++)
+{
 
+	if(b[y - z] != '0' && b[y-z] != '1')
+	{ return (0); }
 
+	if(b[y-z] == '1')
+	{value += x ;}
 
+	x *= 2;
+}
 
-
-
-
-
-
-
+return (value);
 }
