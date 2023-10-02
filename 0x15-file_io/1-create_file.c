3f5c0b054c8c13cc5398b8015236741c036ff2f1
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-02 13:41:30 +0000

    task 101

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-FILE *files;
-files = fopen(filename,"wr")
+int checker = 0;
+int files = 0;
 
+if (filename == NULL)
+{ return (-1); }
 
+files = open(filename, O_RDWR | O_CREAT, 0600);
 
+if (files == -1)
+{ return (-1); }
 
-fclose(files);
-return(1);
+if (text_content != NULL)
+{
+
+checker = write(files, text_content, strlen(text_content));
+if (checker == -1)
+{ return (-1); }
+
+}
+
+close(files);
+return (1);
 }
