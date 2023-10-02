21b1bb30b6be25201191f7d043998432800705b5
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-02 18:34:46 +0000

    task 100

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the elf file of a process
+ * @argv: 0-nameoffile 1-The process
+ * @argc: The number of arguments in the line
+ * Return: 0 if successes,error(98) with a reason if failed
+ */
+
+int main(int argc, char **argv)
+{
+
+dprintf(2,"Error");
+exit(98);
+
+return(0);
+}	
