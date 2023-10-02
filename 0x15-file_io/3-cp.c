b682091eb3560fdf913d308ac5badb2f5f7ccf43
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-02 15:28:11 +0000

    task 101

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,34 +12,28 @@ int main(int argc, char **argv)
 {
 int checker = 0, fileto = 0, filefrom = 0;
 char *L;
-
 L = malloc(1024 * sizeof(char));
-
 if (argc != 3)
 { dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 exit(98); }
-
-filefrom = open(argv[1], O_RDWR);
+filefrom = open(argv[1], O_RDONLY);
 if (filefrom == -1)
 { dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 exit(98); }
-
-fileto = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-if (fileto == -1)
-{ dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
-exit(99); }
-
 checker = read(filefrom, L, 1024);
 if (checker == -1)
 { dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 exit(98); }
-
+fileto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fileto == -1)
+{ dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+free(L);
+exit(99); }
 checker = write(fileto, L, checker);
 if (checker == -1)
 { dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 free(L);
 exit(99); }
-
 free(L);
 checker = close(filefrom);
 if (checker == -1)
