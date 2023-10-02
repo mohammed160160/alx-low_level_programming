2207d06dac47b1d87bfc720b76f9278c3c656ae6
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-02 14:59:39 +0000

    task 100

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  * the reason of exiting to standard error
  */
 
-int cp(int argc, char **argv)
+int main(int argc, char **argv)
 {
 int checker = 0, fileto = 0, filefrom = 0;
 char *L;
@@ -24,7 +24,7 @@ if (filefrom == -1)
 { dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 exit(98); }
 
-fileto = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0661);
+fileto = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 if (fileto == -1)
 { dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 exit(99); }
