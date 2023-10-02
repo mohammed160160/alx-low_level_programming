3ec35089b112fea3a750e95a80154c02e0eea7fe
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-02 17:05:58 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,40 +13,30 @@ int main(int argc, char **argv)
 ssize_t checker = 0;
 int fileto = 0, filefrom = 0;
 char *L;
-
 if (argc != 3) /*Checks if there's 2 other arguments*/
 { dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97); }
-/************************************************************/
 L = malloc(1024 * sizeof(char)); /*Creates a buffer of 1024*/
 if (L == NULL)
 { Errors(2, argv[2]); }
-/********************************************************/
-filefrom = open(argv[1], O_RDONLY);
+filefrom = open(argv[1], O_RDONLY); /*Opening the fromfile*/
 if (filefrom == -1)
 { Errors(1, argv[1]); }
-checker = read(filefrom, L, 1024);
+checker = read(filefrom, L, 1024); /*reading the first 1024 bytes from file*/
 if (checker == -1)
 { Errors(1, argv[1]); }
-fileto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+fileto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /*Opening tofile*/
 if (fileto == -1)
 { free(L);
 Errors(2, argv[2]); }
-
-while (checker > 0)
+while (checker > 0)/*A loop in case fromfile has more than 1024 bits*/
 {
-checker = write(fileto, L, checker);
+checker = write(fileto, L, checker);/*write the content in buffer to fileto*/
 if (checker == -1)
 { free(L);
 Errors(2, argv[2]); }
-
-checker = read(filefrom, L, 1024);
-
-fileto = open(argv[2], O_WRONLY | O_APPEND);
-
-if (fileto == -1)
-{ free(L);
-Errors(2, argv[2]); }
+checker = read(filefrom, L, 1024); /*Reads the rest of the fromfile*/
+fileto = open(argv[2], O_WRONLY | O_APPEND);/*opensthefileatit'sapprend*/
 }
 /**********************************************************/
 free(L);
