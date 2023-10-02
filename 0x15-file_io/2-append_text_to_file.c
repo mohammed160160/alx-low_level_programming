0b6d277aa4a38105a0d5362c3bd73cb3802a68b2
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-02 14:01:41 +0000

    tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - appends text at the end of a file.
+ * append_text_to_file - appends text at the end of a file.
  * @filename: The name of the file in question
  * @text_content: NULL terminated string to add at the end of the file
  * Return: 1 on success or -1 if any conditon failed
