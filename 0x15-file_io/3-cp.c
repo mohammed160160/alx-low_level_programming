c9a3b114a9035108201fa71c7d1a21b579efd455
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-02 15:48:47 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,34 +14,34 @@ int checker = 0, fileto = 0, filefrom = 0;
 char *L;
 L = malloc(1024 * sizeof(char));
 if (argc != 3)
-{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97); }
 filefrom = open(argv[1], O_RDONLY);
 if (filefrom == -1)
-{ dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98); }
 checker = read(filefrom, L, 1024);
 if (checker == -1)
-{ dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98); }
 fileto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fileto == -1)
-{ dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 free(L);
 exit(99); }
 checker = write(fileto, L, checker);
 if (checker == -1)
-{ dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 free(L);
 exit(99); }
 free(L);
 checker = close(filefrom);
 if (checker == -1)
-{ dprintf(STDERR_FILENO, "Error: Can't read from %i", filefrom);
+{ dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", filefrom);
 exit(100); }
 checker = close(fileto);
 if (checker == -1)
-{ dprintf(STDERR_FILENO, "Error: Can't close fd %i", fileto);
+{ dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fileto);
 exit(100); }
 return (0);
 }
