3010a4f76c8e0798eed040861e481025c6787d33
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-10-05 12:00:28 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - creates a function that does the work of cp function
  * @argv: 0-nameoffile 1-name of fromfile 2-name of tofile
@@ -7,7 +6,6 @@
  * Return: 0 if successes,if it failed for any reason it exits and prints
  * the reason of exiting to standard error
  */
-
 int main(int argc, char **argv)
 {
 int fileto = 0, filefrom = 0, reader = 0, writer = 0;
@@ -16,32 +14,32 @@ char *L;
 if (argc != 3) /*Checks if there's 2 other arguments*/
 { dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97); }
-
 L = malloc(1024 * sizeof(char)); /*Creates a buffer of 1024*/
 if (L == NULL)
-{ Errors(2, argv[2]); }
+{ Errors(99, argv[2]); }
 
 filefrom = open(argv[1], O_RDONLY); /*Opening the fromfile*/
 if (filefrom == -1)
-{ Errors(1, argv[1]); }
+{ Errors(98, argv[1]); }
 reader = read(filefrom, L, 1024); /*reading the first 1024 bytes from file*/
 if (reader == -1)
-{ Errors(1, argv[1]); }
+{ Errors(98, argv[1]); }
+
 fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664); /*Opening tofile*/
 if (fileto == -1)
 { free(L);
-Errors(2, argv[2]); }
+Errors(99, argv[2]); }
 
 while (reader > 0)/*A loop in case fromfile has more than 1024 bits*/
 {
 writer = write(fileto, L, reader);/*write the content in buffer to fileto*/
 if (writer == -1)
 { free(L);
-Errors(2, argv[2]); }
+Errors(99, argv[2]); }
 
 reader = read(filefrom, L, 1024); /*Reads the rest of the fromfile*/
 if (reader == -1)
-{ Errors(1, argv[1]); }
+{ Errors(98, argv[1]); }
 }
 
 free(L);
@@ -72,9 +70,9 @@ void filecloser(int file)
  */
 void Errors(int checker, char *file)
 {
-if (checker == 1)
+if (checker == 98)
 	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file); }
 else
 	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file); }
-	exit(97 + checker);
+	exit(checker);
 }
