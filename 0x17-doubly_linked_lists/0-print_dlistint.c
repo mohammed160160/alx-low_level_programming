636db8412079d791f936daf9aeb38504fe7dba72
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-09 07:47:06 +0000

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a double listint_t list
+ * @h:A pointer to the first element of the list
+ * Return: The number of elements in the linked list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+unsigned int length = 0;
+
+for (length = 0; h != NULL ; length++)
+{
+printf("%i\n", h->n);
+h = h->next;
+}
+
+return (length);
+}
