fa9c9390588dc33ecbebff4d051293874842ddfd
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-13 11:19:21 +0000

    task 100

---
+++ 0x18-dynamic_libraries/100-calculations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ * op_add - this operation is to add 2 integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The value of adding a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - this operation is to subtract 2 integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The value of subtracting a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - this operation is to multiply 2 integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The value of multiplying a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - this operation is to divide 2 integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The value of dividing a and b without the remainder
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - this operation is to find the remainder of dividing 2 integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The value of remainder of dividing a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
