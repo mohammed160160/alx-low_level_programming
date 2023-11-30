bbd5638a52e0b9ab99112d9e1bac2b6a4ce66ea8
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-30 15:36:51 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hash;
+unsigned long int i;
 
-hash = (hash_table_t *) malloc(sizeof(hash_table_t));
-
+hash = malloc(sizeof(hash_table_t));
 if (hash == NULL)
 { return (NULL); }
 
 hash->size = size;
-hash->array = (hash_node_t **) calloc(hash->size, sizeof(hash_node_t *));
+
+hash->array = malloc(sizeof(hash_node_t *) * size);
 
 if (hash->array == NULL)
 {
 free(hash);
 return (NULL);
 }
+
+for (i = 0; i < size; i++)
+{ hash->array[i] = NULL; }
 
 return (hash);
 }
