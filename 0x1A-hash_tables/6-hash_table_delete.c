1d45b4d6321a2f4833a3a5cf9fa10a3350fbb2d0
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-30 18:48:18 +0000

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,14 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
- *
+ * hash_table_delete - deletes a hash table
+ * @ht:A hash table
  * Return: Always EXIT_SUCCESS.
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
+hash_node_t *element;
+unsigned long int size;
 
+if (ht == NULL) /*checking parameters*/
+{ return; }
+
+for (size = 0; size < ht->size; size++)
+{
+element = ht->array[size];
+
+if (element != NULL)
+{
+free(element->key);
+free(element->value);
+free(element);
+}
 
 }
-~  
+
+free(ht->array);
+free(ht);
+}
