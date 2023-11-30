ca646dffb1381d9fd22c9ea52776aa526ddf0da1
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-30 16:44:53 +0000

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,13 +1,41 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
- *
+ * hash_table_get - gets a value of the key in the hash table
+ * @ht: The hash table
+ * @key: The key to get the value for
  * Return: Always EXIT_SUCCESS.
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+hash_node_t *element;
+unsigned long int index;
 
+if (ht == NULL || key == NULL) /*checking parameters*/
+{ return (NULL); }
 
+if (*key == '\0') /*checking if the string in key is empty or not*/
+{
+return (NULL);
+}
+
+index = key_index((const unsigned char *)key, ht->size);
+if (index >= ht->size)
+{
+return (NULL);
+}
+
+element = ht->array[index];
+
+while (element != NULL)
+{
+	if (strcmp(element->key, key) == 0)
+	{
+	return (element->value);
+	}
+element = element->next;
+}
+
+return (NULL);
 }
