a6998e26a802be649fcff1012b9319741e8a2132
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-11-30 15:57:46 +0000

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,6 +8,17 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+char *valuecopy;
 
+if (ht == NULL || key = NULL || value == NULL)
+{ return(0); }
 
+if (*key == '\0')
+{ return(0); }
+
+valuecopy = strdup(value);
+
+index = key_index((const unsigned char *)key, ht->size);
+
+return (1);
 }
